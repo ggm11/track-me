3437a4b6739903139258d75fbd51839e98bcf750
gabriel <gabrigm90.11@gmail.com>
2022-11-09 17:32:59 +0100

    login form

--- t-ui/src/pages/Login.tsx
+++ t-ui/src/pages/Login.tsx
@@ -1,5 +1,30 @@
+import { useState } from "react";
+
 function Login() {
-  return <h1>LOGIN</h1>;
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const submitDisabled = !Boolean(username.length > 6 && password.length > 6);
+
+  const handleSubmit = () => console.log({ username, password });
+
+  return (
+    <>
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="username"></label>
+        <input
+          id="username"
+          onChange={(event) => setUsername(event.target.value)}
+        />
+        <label htmlFor="password"></label>
+        <input
+          id="password"
+          onChange={(event) => setPassword(event.target.value)}
+        />
+        <input type="submit" value="Submit" disabled={submitDisabled} />
+      </form>
+    </>
+  );
 }
 
 export default Login;
